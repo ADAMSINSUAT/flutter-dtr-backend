c9568cc5d4ec7e4cb92701b2f5483d36d028d2e4
Your name <you@example.com>
2023-09-29 16:52:49 +0800

    Added CORS policy

--- Program.cs
+++ Program.cs
@@ -13,6 +13,18 @@ builder.Services.AddSwaggerGen();
 //Connection string to the database
 builder.Services.AddDbContext<SampleDtrDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("ConnString")));
 
+//Sets up CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+                          policy =>
+                          {
+                              policy.WithOrigins("192.168.131.93:44341")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                          });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +35,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//To allow using the CORS Policy
+app.UseCors("AllowLocalhost");
 
 app.UseAuthorization();
 
